4b9823a66c64f35f685cc83a9c762790345e384b
Sekhat Temporus <sekhat@temporus.me>
2020-02-11 18:48:15 +0000

    Add layout componentThe layout component should allow us to encapsulate the general look ofthe page.

---
+++ src/Components/Layout.tsx
@@ -0,0 +1,20 @@
+import React, { PropsWithChildren } from "react";
+import styles from "./layout.module.css";
+
+const Layout = (props: PropsWithChildren<{}>) => {
+    return (
+        <div className={styles.app}>
+            <header>
+
+            </header>
+            <main>
+                {props.children}
+            </main>
+            <footer>
+
+            </footer>
+        </div>
+    )
+};
+
+export default Layout;

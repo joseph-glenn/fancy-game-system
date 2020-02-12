efae9ccc3dfeb511ec0e5829d9b0c8e1d7354b4b
Joseph Glenn <Joe.Glenn@Ideagen.com>
2020-02-12 11:09:04 +0000

    Add linting and code formatting
    - also included VS Code extension recommendations.

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import Layout from './Components/Layout';
 import './App.css';
 
-
 const App = () => {
-  return (
-    <Layout>
-      <div>Hello World!</div>
-    </Layout>
-  );
-}
+    return (
+        <Layout>
+            <div>Hello World!</div>
+        </Layout>
+    );
+};
 
 export default App;

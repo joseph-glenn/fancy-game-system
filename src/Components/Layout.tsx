18dfe0cbf6fa8580b8a909b2a0ff2e75dd272052
Joseph Glenn <Joe.Glenn@Ideagen.com>
2020-02-12 11:09:04 +0000

    Add linting and code formatting
    - also included VS Code extension recommendations.

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,20 +1,14 @@
-import React, { PropsWithChildren } from "react";
-import styles from "./layout.module.css";
+import React, { PropsWithChildren } from 'react';
+import styles from './layout.module.css';
 
 const Layout = (props: PropsWithChildren<{}>) => {
     return (
         <div className={styles.app}>
-            <header>
-
-            </header>
-            <main>
-                {props.children}
-            </main>
-            <footer>
-
-            </footer>
+            <header></header>
+            <main>{props.children}</main>
+            <footer></footer>
         </div>
-    )
+    );
 };
 
 export default Layout;

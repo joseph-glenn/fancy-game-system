de6b9d04be39b1f8d7ac61b6f9c49a2d05d065c2
Joseph Glenn <Joe.Glenn@Ideagen.com>
2020-02-13 10:43:49 +0000

    Fix eslint npm script operation

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,7 +1,7 @@
 import React, { PropsWithChildren } from 'react';
-import styles from './layout.module.css';
+import styles from './Layout.module.css';
 
-const Layout = (props: PropsWithChildren<{}>) => {
+const Layout = (props: PropsWithChildren<{}>): JSX.Element => {
     return (
         <div className={styles.app}>
             <header></header>
